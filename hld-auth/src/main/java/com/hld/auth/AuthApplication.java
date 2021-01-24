72ec15a834595304f2564e86cf8e4f91271a00c6
何辉 <hjj81828hjj@163.com>
2021-01-24 19:42:57 +0800

    初次提交

---
+++ hld-auth/src/main/java/com/hld/auth/AuthApplication.java
@@ -0,0 +1,16 @@
+package com.hld.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class AuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApplication.class, args);
+    }
+
+}
+
